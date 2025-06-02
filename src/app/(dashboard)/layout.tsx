39c3a4a0f2a35ed8aee1561735ed7e1f6a6e5855
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-02 17:51:47 -0600

    add:  sidebar component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,12 @@
 import { SidebarProvider } from "@meet/components/ui/sidebar";
+import DashboardNavbar from "@meet/modules/dashboard/ui/components/dashboard-navbar";
 import DashboardSidebar from "@meet/modules/dashboard/ui/components/dashboard-siderbar";
 
 const Layout = ({ children } : { children: React.ReactNode }) => (
   <SidebarProvider>
     <DashboardSidebar />
     <main className="flex flex-col h-screen w-screen bg-muted">
+      <DashboardNavbar />
       {children}
     </main>
   </SidebarProvider>
