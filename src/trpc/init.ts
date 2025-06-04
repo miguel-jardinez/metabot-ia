b0638a7a348a18f4c2a293a862ad2340514f0559
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-04 17:13:34 -0600

    feat: create protected procedure

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,5 +1,8 @@
 import { cache } from "react";
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
+import { headers } from "next/headers";
+
+import { auth } from "@meet/utils/auth";
 export const createTRPCContext = cache(async () => 
 
   /**
@@ -22,3 +25,14 @@ const t = initTRPC.create({
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+export const protectedProcedure = baseProcedure.use( async ({ ctx, next }) => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+
+  if (!session) {
+    throw new TRPCError({ code: "UNAUTHORIZED", message: "You must be logged in to access this resource." });
+  }
+
+  return next({ ctx: { ...ctx, auth: session } });
+});
