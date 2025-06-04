13c2fec5a2c91aa1fe9f411cdb646135bbb48f24
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-03 18:06:56 -0600

    testing route

--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,27 +1,9 @@
 "use client";
 
-import { Button } from "@meet/components/ui/button";
-import { authClient } from "@meet/utils/auth-client";
-
-const HomeView = () => {
-  const { data: session } = authClient.useSession();
-
-  if (!session) {
-    return (
-      <div>
-        You are not logged in
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <p>Logged in as {session?.user.name}</p>
-      <Button onClick={() => authClient.signOut()} className="mt-4">
-        Sign out
-      </Button>
-    </div>
-  );
-};
+const HomeView = () => (
+  <div>
+    Home View 
+  </div>
+);
  
 export default HomeView;
