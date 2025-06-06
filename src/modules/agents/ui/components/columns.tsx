15c7c67dd652443d5a755e6e5fd626316b35acbf
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-05 20:32:07 -0600

    feat: data table created

--- src/modules/agents/ui/components/columns.tsx
+++ src/modules/agents/ui/components/columns.tsx
@@ -1,27 +1,43 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { CornerDownRightIcon, VideoIcon } from "lucide-react";
 
-// This type is used to define the shape of our data.
-// You can use a Zod schema here if you want.
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
-};
+import GeneratedAvatar from "@meet/components/generated-avatar";
+import { Badge } from "@meet/components/ui/badge";
 
-export const columns: ColumnDef<Payment>[] = [
+import { AgentGetOne } from "../../types";
+
+export const columns: ColumnDef<AgentGetOne>[] = [
   {
-    accessorKey: "status",
-    header: "Status"
+    accessorKey: "name",
+    header: "Agent name",
+    cell: ({ row }) => (
+      <div className="flex flex-col gap-y-1">
+        <div className="flex items-center gap-x-2">
+          <GeneratedAvatar variant="botttsNeutral" seed={row.original.name} />
+          <span className="font-semibold capitalize">
+            {row.original.name}
+          </span>
+        </div>
+        
+        <div className="flex items-center gap-x-2">
+          <CornerDownRightIcon className="size-3 text-muted-foreground" />
+          <span className="text-sm text-muted-foreground max-w-[200px] truncate capitalize">
+            {row.original.instructions}
+          </span>
+        </div>  
+      </div>
+    )
   },
   {
-    accessorKey: "email",
-    header: "Email"
-  },
-  {
-    accessorKey: "amount",
-    header: "Amount"
+    accessorKey: "meetingCount",
+    header: "Meetings",
+    cell: () => (
+      <Badge variant="outline" className="flex items-center gap-x-2 [&>svg]:size-4">
+        <VideoIcon className="text-blue-700" />
+        5 Meetings
+      </Badge>
+    )
   }
 ];
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -2,15 +2,22 @@
 
 import { useSuspenseQuery } from "@tanstack/react-query";
 
+import EmptyState from "@meet/components/empty-state";
 import { useTRPC } from "@meet/trpc/client";
+
+import { columns } from "../components/columns";
+import { DataTable } from "../components/data-table";
 
 const AgentsView = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <div>
-      {JSON.stringify(data, null, 2)}
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {
+        data.length === 0 && <EmptyState title="Create your first agent" description="Create and agent to join your meeting. Each agent will follow yout instructions and can interact with participants during the call" />
+      }
     </div>
   );
 };
