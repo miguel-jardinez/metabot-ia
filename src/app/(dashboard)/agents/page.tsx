58f8a474450c31f1aeb21d20f0d680bd1f5e4b37
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-06 18:35:08 -0600

    update ato works with filter

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -2,17 +2,25 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { SearchParams } from "nuqs";
 
 import ErrorState from "@meet/components/error-state";
 import LoadingState from "@meet/components/loading-state";
+import { loadSearchParams } from "@meet/modules/agents/params";
 import ListAgentHeader from "@meet/modules/agents/ui/components/list-agent-header";
 import AgentsView from "@meet/modules/agents/ui/views/agents-view";
 import { getQueryClient, trpc } from "@meet/trpc/server";
 
-const Page = async () => {
-  const queryClient = getQueryClient();
+interface PageProps {
+  searchParams: Promise<SearchParams>
+}
 
-  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+const Page = async ({ searchParams } : PageProps) => {
+  const filters = await loadSearchParams(searchParams);
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+    ...filters
+  }));
 
   return (
     <>
