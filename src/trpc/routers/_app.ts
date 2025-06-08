63eaedd580655fd1c5bb4cb3e17df8b32135dbf7
Miguel Jardinez Ramos <jardinez.ramos.miguel@gmail.com>
2025-06-08 16:25:13 -0600

    add meeting procedure

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,11 @@
 import { agentsRouter } from "@meet/modules/agents/server/procedures";
+import { meetingsRouter } from "@meet/modules/meetings/server/procedures";
 
 import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
-  agents: agentsRouter
+  agents: agentsRouter,
+  meetings: meetingsRouter
 });
 
 // export type definition of API
